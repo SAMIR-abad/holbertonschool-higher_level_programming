ef8d88dfdcfcd2116754753bad119059f7ed4be0
SAMIR-abad <13159@holbertonstudents.com>
2026-03-11 09:16:16 +0000

    "

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
+a = 1
+b = 2
+
 if __name__ == "__main__":
-    print("1 + 2 = {c}".format(c = add(1,2)))
+    print("{a} + {b} = {c}".format(a = a, b = b, c = add(a,b)))
