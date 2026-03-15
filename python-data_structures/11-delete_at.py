70d80c61837182d424a6c49e93bdd708cd934c3a
SAMIR-abad <13159@holbertonstudents.com>
2026-03-15 13:49:21 +0000

    "

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-delete_at(my_list=[], idx=0):
+def delete_at(my_list=[], idx=0):
+    """Specific position-dən elementi silən funksiya"""
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return my_list
