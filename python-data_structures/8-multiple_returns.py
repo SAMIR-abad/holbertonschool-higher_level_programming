cc2c8f2b94ffa27737c5d960e0cd5cb48f4932d0
SAMIR-abad <13159@holbertonstudents.com>
2026-03-15 12:50:35 +0000

    "

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,6 +4,5 @@ def multiple_returns(sentence):
     if length == 0:
         first_char = None
     else:
-        first_char = sentence[0]
-        
+        first_char = sentence[0]     
     return (length, first_char)
