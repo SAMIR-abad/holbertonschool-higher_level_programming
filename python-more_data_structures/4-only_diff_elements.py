9194f54bf346dc9a87dfef7d39ecb7124616122e
SAMIR-abad <13159@holbertonstudents.com>
2026-03-21 19:15:52 +0000

    s

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,9 +3,9 @@ def only_diff_elements(set_1, set_2):
     lst_1, lst_2 = set_1, set_2
     result = []
     for el_1 in lst_1:
-        if not el_1 in lst_2:
+        if el_1 not in lst_2:
             result.append(el_1)
     for el_2 in lst_2:
-        if not el_2 in lst_1:
+        if el_2 not in lst_1:
             result.append(el_2)
     return set(result)
