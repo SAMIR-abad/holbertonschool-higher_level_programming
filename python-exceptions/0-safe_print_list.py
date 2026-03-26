c9d556662457d636e4781f0b341abee958004fdf
SAMIR-abad <13159@holbertonstudents.com>
2026-03-26 19:42:04 +0000

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        if not (isinstance(x, int) or x <= 0 or isinstance(my_list, list)):
-            raise ValueError
-        result = ''
-        for idx, value in enumerate(my_list):
-            if idx < x:
-                result += str(value)
-            else:
-                break
-        result = int(result)
-        return result
-    except:
-        return 0
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return count
