074ae5d5ea736f6dd213c4dbdba918d69c903633
SAMIR-abad <13159@holbertonstudents.com>
2026-03-28 16:23:31 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,4 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 class Square:
-    def __init__(self, size=0):
-        self.size = size
-    @property
-    def size(self):
-        return self.__size
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+    def __init__(self, square):
+        self.__square = square
