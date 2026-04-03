72522e52916657bf7e5f872b4bcbf4752c575f05
SAMIR-abad <13159@holbertonstudents.com>
2026-04-03 20:09:37 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-class Square():
-    """This class defines a rectangle by
-    its properties and future behaviors."""
-    pass
+if __name__ = "__main__":
+    class Square():
+        """This class defines a rectangle by
+        its properties and future behaviors."""
+        pass
