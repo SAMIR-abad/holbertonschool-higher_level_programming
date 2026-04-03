e85af7e0c72716aaaf0542066511cc1fb8ae4f79
SAMIR-abad <13159@holbertonstudents.com>
2026-04-03 19:43:10 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class that represents a rectangle shape."""
-
-
 class Rectangle:
-    """This class defines a rectangle by
-    its properties and future behaviors."""
     pass
