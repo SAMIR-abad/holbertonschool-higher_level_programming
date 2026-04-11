d16f34abf2a84e067fe79c507a4e4531ad7ab69a
SAMIR-abad <13159@holbertonstudents.com>
2026-04-11 17:38:22 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Kvadrat sinfini təyin edən modul."""
+
+
 class Square:
+    """Kvadratı təmsil edən sinif."""
+
     def __init__(self, size):
+        """Kvadrat obyektini inisializasiya edir.
+
+        Args:
+            size: Kvadratın ölçüsü (tərəfi).
+        """
         self.__size = size
