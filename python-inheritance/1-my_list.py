74982720e4c23f6b3439d91a4501b8643b76400c
SAMIR-abad <13159@holbertonstudents.com>
2026-04-11 18:19:00 +0400

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""list sinfindən miras alan MyList modulunu təyin edir."""
+"""
+This module contains the MyList class.
+"""
 
 
 class MyList(list):
-    """Standart list-ə əlavə funksionallıq verən sinif."""
+    """A class that inherits from list."""
 
     def print_sorted(self):
-        """Siyahını artan sıra ilə çap edir, lakin orijinalı dəyişmir."""
+        """
+        Prints the list elements in ascending order.
+        Assumes all elements in the list are integers.
+        """
         print(sorted(self))
