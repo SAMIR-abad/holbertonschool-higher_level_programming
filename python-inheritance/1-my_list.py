1f8c5bf0a55f0866f8a39072fbcc6002066ccca4
SAMIR-abad <13159@holbertonstudents.com>
2026-04-12 12:30:09 +0400

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""
-Bu modul MyList sinfini təyin edir.
-"""
-
-
 class MyList(list):
-    """
-    Siyahı obyektindən miras alan və əlavə funksionallığı olan sinif.
-    """
-
     def print_sorted(self):
-        """
-        Siyahıdakı tam ədədləri artan sıra ilə çap edir.
-        """
         print(sorted(self))
