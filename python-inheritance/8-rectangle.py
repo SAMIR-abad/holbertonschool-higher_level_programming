469b6d1e11e298f7389c9fd33ad992a6c3e18ac8
SAMIR-abad <13159@holbertonstudents.com>
2026-04-16 16:07:03 +0400

    Update 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+"""BaseGeometry-dən miras alan Rectangle sinfi."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    """Düzbucaqlı təmsil edən sinif."""
+
+    def __init__(self, width, height):
+        """Yeni bir Rectangle instansiyası yaradır.
+
+        Args:
+            width (int): Düzbucaqlının eni.
+            height (int): Düzbucaqlının hündürlüyü.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
