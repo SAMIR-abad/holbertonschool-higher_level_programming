9b2c2a57ef30e5d7a8d24758e79dc359fd262f2e
SAMIR-abad <13159@holbertonstudents.com>
2026-04-16 15:44:36 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,9 +5,13 @@
 class BaseGeometry:
     """Həndəsə fiqurları üçün təməl sinif."""
 
-    def integer_validator(self, name, value):
+    def area(self):
+        """Hələlik tətbiq edilməmiş sahə metodu."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, age):
         """Tam ədədləri yoxlayan metod"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(age))
