ca9fbc96b3858f6db11a52a3a52b38c4d08f60be
SAMIR-abad <13159@holbertonstudents.com>
2026-04-16 15:34:47 +0400

    Create 7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry sinfini təyin edən modul."""
+
+
+class BaseGeometry:
+    """Həndəsə fiqurları üçün təməl sinif."""
+
+    def area(self):
+        """Hələlik tətbiq edilməmiş sahə metodu."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Tam ədədləri yoxlayan metod"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
