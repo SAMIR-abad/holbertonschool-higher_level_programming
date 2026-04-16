7732b77c460cd6260b1082a39a90ee2594c9a777
SAMIR-abad <13159@holbertonstudents.com>
2026-04-16 15:30:13 +0400

    Update 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+"""Obyektin tipini və miras almanı yoxlayan modul."""
 
+
+def is_kind_of_class(obj, a_class):
+    """Obyektin sinif və ya miras uyğunluğunu yoxlayır"""
+    return isinstance(obj, a_class)
