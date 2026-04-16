7b3704a9299c7969bec47f9334a0479d80ba5fde
SAMIR-abad <13159@holbertonstudents.com>
2026-04-16 15:16:22 +0400

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-"""izah."""
-
-
+"""Explain"""
 class MyList(list):
-    """izah"""
-
-    def __init__(self, *args):
-        """izah"""
-
-        if len(args) > 1:
-            raise TypeError(
-                "list() takes at most 1 argument ({} given)".format(len(args))
-            )
-        super().__init__(*args)
-
+    """Explain"""
     def print_sorted(self):
-        """izah"""
-
-        try:
-            print(sorted(self))
-        except TypeError:
-            raise TypeError("unorderable types: str() < int()")
+        """Explain"""
+        print(sorted(self))
