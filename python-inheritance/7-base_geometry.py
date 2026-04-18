1095f6456acffc9d5d4b6ff0e03b55f48cec51e7
SAMIR-abad <13159@holbertonstudents.com>
2026-04-19 00:53:54 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""BaseGeometry modulu."""
+"""BaseGeometry sinfi üçün modul."""
 
 
 class BaseGeometry:
-    """Həndəsə üçün baza sinfi."""
+    """Həndəsə fiqurları üçün baza sinfi."""
 
     def area(self):
-        """Hələlik tətbiq edilməyib."""
+        """Hələlik tətbiq edilməmiş sahə metodu."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Dəyərin tam ədəd və 0-dan böyük olmasını yoxlayır.
-        
-        Qeyd: type() istifadə edirik, çünki isinstance(True, int) True qaytarır,
-        amma biz boolean-ları qəbul etməməliyik.
-        """
+        """Dəyərin tam ədəd və müsbət olmasını yoxlayır."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
