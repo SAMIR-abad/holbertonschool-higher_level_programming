26b4ea0e732877b09de7e2dcb9c6539bdd35bcf7
SAMIR-abad <13159@holbertonstudents.com>
2026-04-19 23:23:16 +0400

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Module for save_to_json_file function."""
+"""Module for load_from_json_file function."""
 import json
-"""ssss"""
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON representation."""
-    with open(filename, "w", encoding="utf-8") as f:
-        """aaaaa"""
-        json.dump(my_obj, f)
+
+def load_from_json_file(filename):
+    """Creates a Python object from a JSON file."""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
