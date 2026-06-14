1650b032b4e62d3f13615c9ba38099a91cb3e7dc
SAMIR-abad <13159@holbertonstudents.com>
2026-06-14 20:49:34 +0400

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib"""
+#!/usr/bin/env python3
+"""urllib kitabxanası ilə verilmiş URL-dən status məlumatını götürən modul."""
 import urllib.request
 
-url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf-8')))
