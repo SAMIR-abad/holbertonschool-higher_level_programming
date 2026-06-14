9e0582c53d5205450b445ab76080859b218dade7
SAMIR-abad <13159@holbertonstudents.com>
2026-06-14 17:24:11 +0000

    s

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""URL-ə email parametri ilə POST sorğusu göndərən modul."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
