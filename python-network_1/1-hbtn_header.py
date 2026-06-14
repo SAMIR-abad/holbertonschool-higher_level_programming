54bf24fe64df6e1bafa3dd664fbcc539c4e3678f
SAMIR-abad <13159@holbertonstudents.com>
2026-06-14 20:57:11 +0400

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-"""Fetches the X-Request-Id header from a given URL"""
-import urllib.request
+#!/usr/bin/env python3
+"""URL-ə sorğu göndərən və X-Request-Id başlıq dəyərini çap edən modul."""
 import sys
+import urllib.request
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
