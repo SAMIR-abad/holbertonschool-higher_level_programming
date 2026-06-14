1d75843d033a350efd373bb7df2e0b4e11f7577e
SAMIR-abad <13159@holbertonstudents.com>
2026-06-14 20:41:58 +0400

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ url = "https://intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     body = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode("utf-8")))
