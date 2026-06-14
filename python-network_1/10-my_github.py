57dc3c2a7de6ae49f76f1823e1e56421a81f3d6e
SAMIR-abad <13159@holbertonstudents.com>
2026-06-14 17:27:45 +0000

    s

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""GitHub API vasitəsilə istifadəçinin daxili ID-sini gətirən modul."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+    try:
+        json_data = response.json()
+        print(json_data.get('id'))
+    except ValueError:
+        print("None")
